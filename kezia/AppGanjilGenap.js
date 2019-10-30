402941ae6b3d51990df572cf8c73c8e8f2da1e7b
kezyagth <kezyagatha@gmail.com>
2019-10-30 13:31:49 +0700

    instalasi react-navigasi

--- kezia/AppGanjilGenap.js
+++ kezia/AppGanjilGenap.js
@@ -1,27 +1,67 @@
 import React, {Component} from 'react';
-import {StyleSheet, View, Text,TextInput,Button} from 'react-native';
+import {StyleSheet, View, Text,TextInput,Button,FlatList} from 'react-native';
 import {bindActionCreators} from 'redux'; //import component redux
 import {connect} from 'react-redux'; //connect React Component to redux Store
 //panggil fungsi action yang sudah dibuat sebelum {actionGanjilGenap}
-import {actionGanjiGenap} from './actions/index'
+import {actionGanjilGenap} from './actions/index'
 
 class AppGanjilGenap extends Component {
 
-    prosesSubmit = () => {
-        alert('oke');
+    state = {
+        inputNumber:''
     }
+
+    //fungsi untuk menampung nilai state inputNumber
+    numberChangeHandler = value => {
+        this.setState({
+            inputNumber: value
+        })
+    }
+
+    numberOutput=() => {
+        return(
+            <FlatList
+                data={this.props.listNumberFormReducer.listInputan}
+                keyExtractor={(item, index) => index.toString()} 
+                renderItem={info => <Text>{info.item.value}</Text>
+                }
+             />
+        );
+    }
+    prosesSubmit = () => {
+        //kirim parameter ke actionGanjilGenap
+        let inputanUser = this.state.inputNumber;
+        let message =''
+        if(inputanUser %2 == 0){
+            message = inputanUser + 'Bernilai Genap';
+        }else if(inputanUser %2 == 1){
+            message = inputanUser + 'Bernilai Ganjil'
+        }else{
+            message = inputanUser + 'Bernilai bukan angka'
+        }
+        this.props.actionGanjilGenap(message);
+        // data hasil inputan
+        console.log('dataInputan');
+        let dataInputan = this.props.listNumberFormReducer;
+        console.log(dataInputan)
+    };
     render()  {
         return(
             <View>
-            <Text>AppGanjilGenap</Text>
-            <TextInput placeholder='input angka'/>
-            <Button title='Proses' onPress={this.prosesSubmit}/>
+                <Text>AppGanjilGenap</Text>
+                <TextInput placeholder='input angka'
+                onChangeText= {this.numberChangeHandler}
+                value={this.state.inputNumber}
+                />
+
+                <Button title='Proses' onPress={this.prosesSubmit} />
+                {this.numberOutput()}
         </View>
         );  
     }
 }
 
-function mapStateToProps(state) {
+function mapStateToProps(state) { //mengambil semua data dari file reducer/index.js
     console.log('Data Store Reducers');
     console.log(state);
     return {
@@ -31,7 +71,7 @@ function mapStateToProps(state) {
 
 function mapDispatchToProps(dispatch){
     return bindActionCreators(
-        {actionGanjiGenap: actionGanjiGenap},dispatch,
+        {actionGanjilGenap: actionGanjilGenap},dispatch,
     )
 }
 export default connect(
